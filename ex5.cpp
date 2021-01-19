aa0ddd2f9f5b3df9659d642ada1ef751bc2d165d
Hannah <hannah.h.hole@gmail.com>
2021-01-19 12:18:01 +0100

    Small change

--- ex5.cpp
+++ ex5.cpp
@@ -3,11 +3,11 @@
 
 #include<iostream>
 using namespace std; 
-int main(){
-    int n, n_term, a=1, b=1; //a is first term, b is second term, n is number of terms, n_term is the nth term
-    cout<<"Enter n: ";
-    cin>>n;
-    for (int i=1; i<=n; i++){ //i is the number of the current term being executed in fibonacci 
+
+//create function 
+int fib(int x)
+{   int a=1, b=1, n_term;      
+    for (int i=1; i<=x; i++){ //i is the number of the current term being executed in fibonacci 
         if(i==1){ //the first term is 1 
             n_term=a;
             continue; //continue lets us execute i==1 AND i==2 
@@ -18,8 +18,16 @@ int main(){
         }
             n_term=a+b;
             a=b; //(n-2)th term. a becomes b. note a=b is NOT same as b=a
-            b=n_term; //(n-1)th term. b becomes last number
-    }
-    cout<<n_term; //output nth term 
-    return 0; 
+            b=n_term; //(n-1)th term. b becomes last number}
+        }
+return n_term;
 }
+
+int main(){
+    int n; //a is first term, b is second term, n is number of terms, n_term is the nth term
+    cout<<"Enter n: ";
+    cin>>n;
+    cout<<"The nth term of the Fibonacci sequence is "<<fib(n); //output nth term 
+    return 0; 
+    }
+
